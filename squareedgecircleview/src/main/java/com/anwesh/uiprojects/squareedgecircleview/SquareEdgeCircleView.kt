c58b52199219ef7606d12039aed1a5cbc62d2097
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 14:51:07 +0530

    adding SECNode class

--- squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
+++ squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
@@ -130,4 +130,48 @@ class SquareEdgeCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SECNode(var i : Int, val state : State = State()) {
+
+        private var next : SECNode? = null
+        private var prev : SECNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SECNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSECNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SECNode {
+            var curr : SECNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
