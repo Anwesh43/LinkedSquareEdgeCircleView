2e9e2cf9909d521be841b28992ba5dda6cee90a9
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 14:02:16 +0530

    adding constants and libraries

--- squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
+++ squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.squareedgecircleview
 /**
  * Created by anweshmishra on 14/08/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
