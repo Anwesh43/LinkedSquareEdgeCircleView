121f193bb01a031caa166582e0b812d4ce75f8a6
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 14:32:53 +0530

    adding Canvas extension method

--- squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
+++ squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
@@ -13,13 +13,15 @@ import android.graphics.Canvas
 import android.graphics.Color
 
 val nodes : Int = 5
-val lines : Int = 4
+val circles : Int = 4
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#311B92")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 4f
+val sqFactor : Float = 2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -30,3 +32,36 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawEdgeCircle(i : Int, scale : Float, r : Float, position : Float, paint : Paint) {
+    val updatingPoisition : Float = position * scale.divideScale(i, circles)
+    save()
+    rotate(90f * i)
+    drawCircle(updatingPoisition, updatingPoisition, r, paint)
+    restore()
+}
+
+fun Canvas.drawSquareEdgeCircle(size : Float, scale : Float, paint : Paint) {
+    val sqSize : Float = size / sqFactor
+    val r : Float = size / rFactor
+    drawRect(-sqSize / 2, -sqSize / 2, sqSize / 2, sqSize / 2, paint)
+    for (j in 0..(circles - 1)) {
+        drawEdgeCircle(j, scale, r, sqSize / 2, paint)
+    }
+}
+
+fun Canvas.drawSECNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * scale.divideScale(1, 2))
+    drawSquareEdgeCircle(size, scale.divideScale(0, 2), paint)
+    restore()
+}
