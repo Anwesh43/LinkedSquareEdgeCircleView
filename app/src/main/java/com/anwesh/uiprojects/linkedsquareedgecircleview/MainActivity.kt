b8aeba380600b5450c98b9716cf2ba5a29acffaa
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 15:16:04 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquareedgecircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquareedgecircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquareedgecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squareedgecircleview.SquareEdgeCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareEdgeCircleView.create(this)
     }
 }
