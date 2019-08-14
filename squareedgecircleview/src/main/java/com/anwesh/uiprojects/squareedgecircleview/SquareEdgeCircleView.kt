0db3bdd06afe8225cb395a5aa1411aab837ff03b
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 14:38:16 +0530

    adding SquareEdgeCircleView class

--- squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
+++ squareedgecircleview/src/main/java/com/anwesh/uiprojects/squareedgecircleview/SquareEdgeCircleView.kt
@@ -65,3 +65,21 @@ fun Canvas.drawSECNode(i : Int, scale : Float, paint : Paint) {
     drawSquareEdgeCircle(size, scale.divideScale(0, 2), paint)
     restore()
 }
+
+class SquareEdgeCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
